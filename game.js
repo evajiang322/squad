ff0a414c23c5a3053be153399f2ce97303a47412
Eva Jiang <evajiang322@gmail.com>
2015-08-03 16:18:31 -0400

    deleting the pressed keys WORKSgit add .

--- game.js
+++ game.js
@@ -8,6 +8,8 @@ var ctx = c.getContext("2d");
 
 var alphabet = ["a","b","c","d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var letters_to_delete = [];
+var x_coord = [];
+var y_coord = [];
 
 
 $("canvas").click(function generateRandomBubble(){
@@ -15,12 +17,16 @@ $("canvas").click(function generateRandomBubble(){
   var index = Math.floor((Math.random() * alphabet.length - 1) + 1);
   index = index % alphabet.length;
   var random_letter = alphabet[index];
-  //so the program knows which letters to check for to delete
-  letters_to_delete.push(random_letter);
 
   //picking random coordinates
   var centerx = Math.floor(Math.random() * (canvas.width - 10 + 1)) + 10;
   var centery = Math.floor(Math.random() * (canvas.height - 10 + 1)) + 10;
+
+  //so the program knows which letters to check for to delete and its coordinates
+  letters_to_delete.push(random_letter);
+  x_coord.push(centerx);
+  y_coord.push(centery);
+
   //drawing the circle thing
   ctx.beginPath();
   ctx.arc(centerx+3, centery-4, 10, 0, 2 * Math.PI);
@@ -33,6 +39,22 @@ $(document).keydown(function(event){
   keynum = event.which;
   letter_pressed = String.fromCharCode(keynum).toLowerCase();
   console.log(letter_pressed);
+  //deleting. aka drawing over the thing.
+  for (var i = 0; i < letters_to_delete.length; i++){
+    if (letters_to_delete[i] === letter_pressed){
+      ctx.beginPath();
+      ctx.arc(x_coord[i]+3, y_coord[i]-4, 11, 0, 2 * Math.PI);
+      ctx.fillStyle = "lavender";
+      ctx.strokeStyle = "lavender";
+      ctx.fill();
+      ctx.stroke();
+
+      letters_to_delete.splice(i, 1);
+      x_coord.splice(i, 1);
+      y_coord.splice(i, 1);
+      break;
+    }
+  }
 
 });
 
