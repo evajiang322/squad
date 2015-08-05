306fa4f2902bd396beec3373531897c6f7d402c0
Eva Jiang <evajiang322@gmail.com>
2015-08-05 09:59:49 -0400

    added falling commented

--- game.js
+++ game.js
@@ -40,34 +40,34 @@ function secondPassed() {
 $("#startButtonPic").click(function(){
   $("#startButtonPic").hide();
   startBtnPressed = true;
-  // generating = setInterval('generateRandomBubble()', 1000);
-  // countdownTimer = setInterval('secondPassed()', 1000);
+  generating = setInterval('generateRandomBubble()', 1000);
+  countdownTimer = setInterval('secondPassed()', 1000);
   // countdownBubble = setInterval('decrease_btime()', 1000);
   // bubbleRemoval = setInterval('bubble_removal()', 1000);
 });
 
 //pause and resume button.
-// var isPaused = true;
-// $("#pauseResumeGame").click(function(){
-//   if(startBtnPressed && !isGameOver){
-//     if(isPaused){
-//       $("#pauseResume").html("Resume");
-//       $("#paused").show();
-//       clearInterval(generating);
-//       clearInterval(countdownTimer);
-//       // clearInterval(countdownBubble);
-//       // clearInterval(bubbleRemoval);
-//     }else{
-//       $("#pauseResume").html("Pause");
-//       generating = setInterval('generateRandomBubble()', 1000);
-//       countdownTimer = setInterval('secondPassed()', 1000);
-//       // countdownBubble = setInterval('decrease_btime()', 1000);
-//       // removeTimer = setInterval('remove_bubble()', 1000);
-//       $("#paused").hide();
-//     }
-//     isPaused = !isPaused;
-//   }
-// });
+var isPaused = true;
+$("#pauseResumeGame").click(function(){
+  if(startBtnPressed && !isGameOver){
+    if(isPaused){
+      $("#pauseResume").html("Resume");
+      $("#paused").show();
+      clearInterval(generating);
+      clearInterval(countdownTimer);
+      // clearInterval(countdownBubble);
+      // clearInterval(bubbleRemoval);
+    }else{
+      $("#pauseResume").html("Pause");
+      generating = setInterval('generateRandomBubble()', 1000);
+      countdownTimer = setInterval('secondPassed()', 1000);
+      // countdownBubble = setInterval('decrease_btime()', 1000);
+      // removeTimer = setInterval('remove_bubble()', 1000);
+      $("#paused").hide();
+    }
+    isPaused = !isPaused;
+  }
+});
 
 
 // vvvvvvv Canvasing begins here vvvvvvv
@@ -78,11 +78,10 @@ var alphabet = ["a","b","c","d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n
 var letters_to_delete = [];
 var x_coord = [];
 var y_coord = [];
-var bubble_time = [];
+// var bubble_time = [];
 
 
 function generateRandomBubble(){
-  // console.log("Im clicked");
   //getting a random index number to get a random letter from alphabet
   var index = Math.floor((Math.random() * alphabet.length - 1) + 1);
   index = index % alphabet.length;
@@ -118,11 +117,11 @@ function generateRandomBubble(){
 
 
 $(document).keydown(function(event){
-  // if (isPaused && !isGameOver){
+  if (isPaused && !isGameOver){
     //this gets the keycode and converts the number to a lowercase letter
     keynum = event.which;
     letter_pressed = String.fromCharCode(keynum).toLowerCase();
-    // console.log(letter_pressed);
+
     //deleting. aka drawing over the thing.
     for (var i = 0; i < letters_to_delete.length; i++){
       if (letters_to_delete[i] === letter_pressed){
@@ -150,103 +149,103 @@ $(document).keydown(function(event){
         $("#score_val").html(score_value);
       }
     }
-  //}
+  }
 });
 
 $("#score_val").html(score_value);
 
 //
 //FOR BUBBLES TO FALL DOWN THE CANVAS:
-var falling_list = [];
-var raf = 0
-
-function bubble(random_letter, centerx){
-  this.x = centerx;
-  this.y = 30;
-  this.vy = 2;
-  this.letter = random_letter;
-  this.draw = function(){
-    ctx.beginPath();
-    ctx.arc(this.x+3, this.y-4, 25, 0, 2 * Math.PI);
-    ctx.strokeStyle = "black";
-    ctx.fillStyle = "black";
-    ctx.font = "20px serif";
-    ctx.fillText(random_letter, this.x , this.y);
-    ctx.stroke();
-  }
-}
-
-$("#canvas").click(function generateFallingBubble(){
-  //getting a random index number to get a random letter from alphabet
-  var index = Math.floor((Math.random() * alphabet.length - 1) + 1);
-  index = index % alphabet.length;
-  var random_letter = alphabet[index];
-
-  var centerx = Math.floor(Math.random() * ((canvas.width - 26) - 26 + 1)) + 26;
-
-  //checking to make sure that the coordinates won't overlap each other
-  for (i=0; i < falling_list.length;i++){
-    xdiff = Math.abs(centerx - falling_list[i].x);
-    ydiff = Math.abs(50 - falling_list[i].y);
-    if (xdiff <= 50 && ydiff <= 50){
-      return;
-    }
-  }
-
-  //creating the circle
-  var circle = new bubble(random_letter, centerx);
-  circle.draw();
-  falling_list.push(circle);
-});
-
-//drawing the new position of the circle when it 'moves'
-function draw() {
-  ctx.clearRect(0,0, canvas.width, canvas.height);
-  for (i=0; i< falling_list.length; i++){
-    falling_list[i].draw();
-    falling_list[i].y += falling_list[i].vy;
-  }
-  raf = window.requestAnimationFrame(draw);
-}
-
-//calling the animation:
-raf = window.requestAnimationFrame(draw);
-//stopping the animation:
-//window.cancelAnimationFrame(raf);
-
-$(document).keydown(function(event){
-  // if (isPaused && !isGameOver){
-    //this gets the keycode and converts the number to a lowercase letter
-    keynum = event.which;
-    letter_pressed = String.fromCharCode(keynum).toLowerCase();
-
-    //deleting. aka drawing over the thing.
-    for (var i = 0; i < falling_list.length; i++){
-      if (falling_list[i].letter === letter_pressed){
-        score_value += 100;
-        $("#score_val").html(score_value);
-        ctx.beginPath();
-        ctx.arc(falling_list[i].x+3, falling_list[i].y-4, 26, 0, 2 * Math.PI);
-        ctx.fillStyle = "lavender";
-        ctx.strokeStyle = "lavender";
-        ctx.fill();
-        ctx.stroke();
-
-        falling_list.splice(i, 1);
-        return;
-      }
-      else if(i === falling_list.length - 1 && falling_list[i].letter !== letter_pressed){
-        score_value -= 200;
-        if(score_value <= 0){
-          score_value = 0;
-        }
-        $("#score_val").html(score_value);
-      }
-    }
-  //}
-});
-
-$("#score_val").html(score_value);
+// var falling_list = [];
+// var raf = 0
+//
+// function bubble(random_letter, centerx){
+//   this.x = centerx;
+//   this.y = 30;
+//   this.vy = 2;
+//   this.letter = random_letter;
+//   this.draw = function(){
+//     ctx.beginPath();
+//     ctx.arc(this.x+3, this.y-4, 25, 0, 2 * Math.PI);
+//     ctx.strokeStyle = "black";
+//     ctx.fillStyle = "black";
+//     ctx.font = "20px serif";
+//     ctx.fillText(random_letter, this.x , this.y);
+//     ctx.stroke();
+//   }
+// }
+//
+// $("#canvas").click(function generateFallingBubble(){
+//   //getting a random index number to get a random letter from alphabet
+//   var index = Math.floor((Math.random() * alphabet.length - 1) + 1);
+//   index = index % alphabet.length;
+//   var random_letter = alphabet[index];
+//
+//   var centerx = Math.floor(Math.random() * ((canvas.width - 26) - 26 + 1)) + 26;
+//
+//   //checking to make sure that the coordinates won't overlap each other
+//   for (i=0; i < falling_list.length;i++){
+//     xdiff = Math.abs(centerx - falling_list[i].x);
+//     ydiff = Math.abs(50 - falling_list[i].y);
+//     if (xdiff <= 50 && ydiff <= 50){
+//       return;
+//     }
+//   }
+//
+//   //creating the circle
+//   var circle = new bubble(random_letter, centerx);
+//   circle.draw();
+//   falling_list.push(circle);
+// });
+//
+// //drawing the new position of the circle when it 'moves'
+// function draw() {
+//   ctx.clearRect(0,0, canvas.width, canvas.height);
+//   for (i=0; i< falling_list.length; i++){
+//     falling_list[i].draw();
+//     falling_list[i].y += falling_list[i].vy;
+//   }
+//   raf = window.requestAnimationFrame(draw);
+// }
+//
+// //calling the animation:
+// raf = window.requestAnimationFrame(draw);
+// //stopping the animation:
+// //window.cancelAnimationFrame(raf);
+//
+// $(document).keydown(function(event){
+//   // if (isPaused && !isGameOver){
+//     //this gets the keycode and converts the number to a lowercase letter
+//     keynum = event.which;
+//     letter_pressed = String.fromCharCode(keynum).toLowerCase();
+//
+//     //deleting. aka drawing over the thing.
+//     for (var i = 0; i < falling_list.length; i++){
+//       if (falling_list[i].letter === letter_pressed){
+//         score_value += 100;
+//         $("#score_val").html(score_value);
+//         ctx.beginPath();
+//         ctx.arc(falling_list[i].x+3, falling_list[i].y-4, 26, 0, 2 * Math.PI);
+//         ctx.fillStyle = "lavender";
+//         ctx.strokeStyle = "lavender";
+//         ctx.fill();
+//         ctx.stroke();
+//
+//         falling_list.splice(i, 1);
+//         return;
+//       }
+//       else if(i === falling_list.length - 1 && falling_list[i].letter !== letter_pressed){
+//         score_value -= 200;
+//         if(score_value <= 0){
+//           score_value = 0;
+//         }
+//         $("#score_val").html(score_value);
+//       }
+//     }
+//   //}
+// });
+//
+// $("#score_val").html(score_value);
 
 
 //counting down the seconds for each bubble
